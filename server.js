a06df1799f95d43f62eedd748ba7edd46a840bd4
Dhouha Lamine <dhouha.lamin@gmail.com>
2025-10-25 11:46:08 +0100

    verifying that update happen

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello Le bon developpeur Community ! - new feature 🚀🚀🚀🚀");
+  res.send("Hello Le bon developpeur Community ! - new feature 🚀");
 });
 
 app.get("/health", async (req, res) => {
